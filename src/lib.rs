3a7e1d5de233ceba50a7d268868d7c23855f284d
alecmocatta <alec@mocatta.net>
2019-07-24 15:14:24 +0100

    v0.1.2

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@
 //!
 //! Currently doesn't support Windows.
 
-#![doc(html_root_url = "https://docs.rs/tcp_typed/0.1.1")]
+#![doc(html_root_url = "https://docs.rs/tcp_typed/0.1.2")]
 #![warn(
 	// missing_copy_implementations,
 	// missing_debug_implementations,
